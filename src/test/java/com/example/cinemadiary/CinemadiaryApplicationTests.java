df2d990d4ad8e17df7d2eeeb6c48150de2d076af
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2026-05-11 12:40:37 +0500

    initial commit

---
+++ src/test/java/com/example/cinemadiary/CinemadiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemadiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemadiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
